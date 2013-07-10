40decdcd33fd12c6bd16fcbc6396b95a84532e24
Christof Debaes <cdebaes@gmail.com>
2013-07-10 00:11:20 +0000

    Tried finish homework

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
-var exporess = require('express');
+var express = require('express');
 var fs = require('fs');
 
-var app = express(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    var buf = fs.readFileSync('index.html');
-   response.send(bug.toString('utf8'));
+   response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
